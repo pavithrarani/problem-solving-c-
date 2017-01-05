2b8632c2c39020e9202bf35b957a717cfe966ae1
pavithrarani <pavithra.be08@gmail.com>
2017-01-04 23:23:08 -0800

    to check unsafe position in given pattern

---
+++ TEST/SafeUnsafeTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using problems;
+namespace TEST
+{
+    [TestClass]
+    public class SafeUnsafeTest
+    {
+        [TestMethod]
+        public void ShouldReturnTrueIfSafe()
+        {
+            var obj = new SafeUnsafe();
+            string[] pattern =
+            {
+                "SSUSS", "SSUSSUSS", "SSUSSUSUUUS", "SSSSSSS", "SSUSUUSUUS", "SSUSSUSUUSUUUSUUUUS"
+            };
+            var jump = 1;
+
+            int i;
+            for (i = 0; i < pattern.Length; i++)
+            {
+                var res = obj.split(pattern[i], jump);
+                Assert.AreEqual(true, res);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnFailIfUnsafe()
+        {
+            var obj = new SafeUnsafe();
+            string[] pattern =
+            {
+                "SSU", "SSUSSUSUUUSS", "U", "SSUSSUSUUUUSUUUS", "SSUSSUSUUUSS"
+            };
+            var jump = 1;
+            var start = jump;
+            int i;
+            for (i = 0; i < pattern.Length; i++)
+            {
+                var res = obj.split(pattern[i], jump);
+                Assert.AreEqual(false, res);
+            }
+        }
+    }
+}
---
+++ problems/SafeUnsafe.cs
@@ -0,0 +1,37 @@
+﻿namespace problems
+{
+    public class SafeUnsafe
+    {
+        public bool split(string patt, int jmp)
+        {
+            if ((patt.Length == 1) && (patt[0] == 'S'))
+                return true;
+
+            if (patt[0] == 'U')
+                return false;
+
+            if (patt.Length > 0)
+            {
+                var left = false;
+                var right = false;
+                var center = false;
+                if (jmp > 1)
+                {
+                    var endLenLeft = patt.Length - (jmp - 1);
+                    if (endLenLeft > 0)
+                        left = split(patt.Substring(jmp - 1, endLenLeft), jmp - 1);
+                }
+                var endLenCenter = patt.Length - jmp;
+                if (endLenCenter > 0)
+                    center = split(patt.Substring(jmp, endLenCenter), jmp);
+
+                var endLenRight = patt.Length - (jmp + 1);
+                if (endLenRight > 0)
+                    right = split(patt.Substring(jmp + 1, endLenRight), jmp + 1);
+
+                return left || center || right;
+            }
+            return false;
+        }
+    }
+}
